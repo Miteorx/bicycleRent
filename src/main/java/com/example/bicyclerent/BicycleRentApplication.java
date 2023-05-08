4deb349ca072c8a9ea94986ba0138c72d0097f2e
Miteorx <miteorx@gmail.com>
2023-05-08 19:36:28 +0300

    starting project

---
+++ src/main/java/com/example/bicyclerent/BicycleRentApplication.java
@@ -0,0 +1,13 @@
+package com.example.bicyclerent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicycleRentApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BicycleRentApplication.class, args);
+  }
+
+}
